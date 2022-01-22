887683d3a34194202428d9bb69b435184a8a9c77
Luis Eduardo Santos Costa <luiseduardo70@live.com>
2022-01-22 12:30:23 -0300

    retorno dos dados na rota de boleto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,51 @@
-require('dotenv').config()
+require('dotenv').config();
 const port = process.env.PORT || 3000;
-
 const app = require('../config/app');
 
-app.listen(port, () => console.log(`Executando na porta ${port}`));
+const { verifyBilletHasCharacter, verifyBillet } = require('../routes/boleto');
+
+
+app.get('/boleto/:code', (req, res) => {
+  const { code } = req.params;
+
+  const codeBillet = verifyBilletHasCharacter(code);
+
+  if(!codeBillet) {  //! If codeBillet is 'false' it has only numbers
+    
+    if(code.length === 47) {  //? Boleto Financeiro
+      try {
+        const { barCode, amount, expirationData } = verifyBillet(code);
+        
+        res.status(200).send({
+          barCode: barCode, 
+          amount: amount, 
+          expirationData: expirationData
+        });
+      }
+      catch (err) {
+        console.log(err);
+        res.status(400).send({error: 'Código inválido'});
+      }
+    }
+    else if(code.length === 48) {  //? Boleto de concessionária
+
+    }
+    else {
+      res.status(404).send({status: 'Código de boleto inválido'});
+    }
+  }
+
+  else 
+    res.status(404).send({status: 'Código de boleto inválido'});
+
+});
+
+
+app.get('/', (req, res) => {
+  res.status(200).send({status: 'Tudo ok!'})
+});
+
+
+app.listen(port, () => 
+  console.log(`Executando na porta ${port}`)
+);
