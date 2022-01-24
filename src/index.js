3fc607885b6163e7320a0bfc56bb4d83c116342d
Luis Eduardo Santos Costa <luiseduardo70@live.com>
2022-01-23 23:57:10 -0300

    Adição do erro de resposta para modulos inválidos

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const port = process.env.PORT || 3000;
 const app = require('../config/app');
 
-const { verifyBilletHasCharacter, verifyBillet } = require('../routes/boleto');
+const { verifyBilletHasCharacter, checkBankSlip } = require('../routes/boleto_bancario');
+const { checkTicketDealership } = require('../routes/boleto_concessionaria');
 
 
 app.get('/boleto/:code', (req, res) => {
@@ -10,26 +11,33 @@ app.get('/boleto/:code', (req, res) => {
 
   const codeBillet = verifyBilletHasCharacter(code);
 
-  if(!codeBillet) {  //! If codeBillet is 'false' it has only numbers
+  if(!codeBillet) {  //? If codeBillet is 'false' it has only numbers
     
-    if(code.length === 47) {  //? Boleto Financeiro
+    if(code.length === 47) {  //? financial slip
       try {
-        const { barCode, amount, expirationData } = verifyBillet(code);
+        const { barCode, amount, expirationData } = checkBankSlip(code);
         
-        res.status(200).send({
-          barCode: barCode, 
-          amount: amount, 
-          expirationData: expirationData
-        });
+        if(!barCode || !amount || !expirationData) {
+          res.status(400).send({error: 'Código inválido'});
+        }
+        else {
+          res.status(200).send({
+            barCode: barCode, 
+            amount: amount, 
+            expirationData: expirationData
+          });
+        }
       }
       catch (err) {
         console.log(err);
         res.status(400).send({error: 'Código inválido'});
       }
     }
-    else if(code.length === 48) {  //? Boleto de concessionária
 
+    else if (code.length === 48) {  //? ticket dealer
+      checkTicketDealership(code);
     }
+
     else {
       res.status(404).send({status: 'Código de boleto inválido'});
     }
@@ -41,11 +49,6 @@ app.get('/boleto/:code', (req, res) => {
 });
 
 
-app.get('/', (req, res) => {
-  res.status(200).send({status: 'Tudo ok!'})
-});
+app.get('/', (req, res) => res.status(200).send({status: 'Tudo ok!'}) );
 
-
-app.listen(port, () => 
-  console.log(`Executando na porta ${port}`)
-);
+app.listen(port, () => console.log(`Executando na porta ${port}`) );
